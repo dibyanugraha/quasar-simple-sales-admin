d22fcce0b34de5d196301b02190d625cf8f75941
Ariestya Dibyanugraha <ariestya.dibyanugraha@gmail.com>
2019-08-13 05:47:21 +0700

    Add login and register page

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -12,7 +12,6 @@
       label="E-mail"
       v-model="formData.email"
       type="email"
-      :rules="[ val => isValidEmailAddress(val) || 'Please enter a valid email address.' ]"
       lazy-rules
       ref="email"
     />
@@ -22,10 +21,6 @@
       filled
       :type="isPwd ? 'password' : 'text'"
       label="Password"
-      :rules="[
-          val => !!val || '* Required',
-          val => val.length >= 6 || 'Please enter at least 6 characters.',
-        ]"
       lazy-rules
       ref="password"
     >
@@ -37,10 +32,8 @@
         />
       </template>
     </q-input>
-    <q-toggle 
-      v-model="accept" label="I accept the license and terms." right-label />
     <div>
-      <q-btn label="Register" type="submit" color="primary" />
+      <q-btn label="Login" type="submit" color="primary" />
       <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
     </div>
   </q-form>
@@ -52,40 +45,20 @@ export default {
   data() {
     return {
       isPwd: true,
-      accept: false,
       formData: {
         email: "",
         password: ""
       },
+      register: false
     };
   },
   methods: {
     onSubmit() {
       this.$refs.email.validate();
       this.$refs.password.validate();
-
-      if (this.accept !== true) {
-        this.$q.notify({
-          color: "red-5",
-          textColor: "white",
-          icon: "fas fa-exclamation-triangle",
-          message: "You need to accept the license and terms first"
-        });
-      } else {
-        this.$q.notify({
-          color: "green-4",
-          textColor: "white",
-          icon: "fas fa-check-circle",
-          message: "Submitted"
-        });
-      }
     },
     onReset() {
       (this.formData.email = ""), (this.formData.password = "");
-    },
-    isValidEmailAddress(email) {
-      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      return re.test(String(email).toLowerCase());
     }
   }
 };
--- src/pages/Auth.vue
+++ src/pages/Auth.vue
@@ -1,45 +1,38 @@
 <template>
   <div class="q-pa-md">
     <div style="max-width: 600px">
-      <q-tabs
-        v-model="tab"
-        align="justify"
-        narrow-indicator
-        class="q-mb-lg"
-      >
+      <q-tabs v-model="tab" align="justify" narrow-indicator class="q-mb-lg">
         <q-tab class="text-purple" name="login" label="Login" />
         <q-tab class="text-orange" name="register" label="Register" />
       </q-tabs>
-
-      <div class="q-gutter-y-sm">
-        <q-tab-panels
-          v-model="tab"
-          class="text-white text-center"
-        >
-          <q-tab-panel name="login">
-          </q-tab-panel>
-
-          <q-tab-panel name="register">
-            <register />
-          </q-tab-panel>
-        </q-tab-panels>
-      </div>
+      <q-separator />
+      <q-tab-panels v-model="tab" class="text-center" animated>
+        <q-tab-panel name="login">
+          <login />
+        </q-tab-panel>
+        <q-tab-panel name="register">
+          <register />
+        </q-tab-panel>
+      </q-tab-panels>
     </div>
   </div>
 </template>
 
 <script>
-import register from 'components/Auth/Register.vue'
+import register from "components/Auth/Register.vue";
+import login from "components/Auth/Login.vue";
+
 export default {
-  data () {
+  data() {
     return {
-      tab: 'mails'
-    }
+      tab: "mails"
+    };
   },
   components: {
-    register
+    register,
+    login
   }
-}
+};
 </script>
 
 <style>
