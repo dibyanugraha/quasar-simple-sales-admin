5a9d84d53e3be0ef707942fb47e84c898255167b
Ariestya Dibyanugraha <ariestya.dibyanugraha@gmail.com>
2019-08-24 02:13:40 +0700

    Made filters prettier

--- src/components/filters/table-filter.vue
+++ src/components/filters/table-filter.vue
@@ -1,42 +1,56 @@
 <template>
-    <q-list bordered>
-      <q-expansion-item group="group01" label="Filters" default-opened header-class="text-primary">
-        <q-card v-for="(fieldFilter, key) in componentFilters" 
-          :key="key">
-          <q-card-section dense>
-            <q-input :label="fieldFilter.label" outlined clearable dense :readonly="fieldFilter.readonly" clear-icon="close">
-              <template v-slot:after>
-                <q-btn
-                  flat
-                  round
-                  dense
-                  color="primary"
-                  icon="delete"
-                  @click="console.log('delete clicked')"
-                />
-              </template>
-            </q-input>
-          </q-card-section>
-          <q-card-section dense>
-            <q-btn unelevated color="primary" class="full-width" label="Add" />
-          </q-card-section>
-        </q-card>
-      </q-expansion-item>
-    </q-list>
+  <q-list>
+    <q-expansion-item
+      class="shadow-1 overflow-hidden"
+      style="border-radius: 5px"
+      group="group01"
+      label="Filters"
+      default-opened
+      header-class="bg-primary text-white"
+      expand-icon-class="text-white"
+    >
+      <q-card v-for="(fieldFilter, key) in componentFilters" :key="key">
+        <q-card-section dense>
+          <q-input
+            :label="fieldFilter.label"
+            outlined
+            clearable
+            dense
+            :readonly="fieldFilter.readonly"
+            clear-icon="close"
+          >
+            <template v-slot:after>
+              <q-btn
+                flat
+                round
+                dense
+                color="primary"
+                icon="delete"
+                @click="console.log('delete clicked')"
+              />
+            </template>
+          </q-input>
+        </q-card-section>
+      </q-card>
+      <q-card>
+        <q-card-section>
+          <q-btn unelevated color="primary" class="full-width" label="Add" />
+        </q-card-section>
+      </q-card>
+    </q-expansion-item>
+  </q-list>
 </template>
 
 <script>
-import Vue from 'vue';
+import Vue from "vue";
 
-  export default {
-    name: "table-filter",
-    props: [
-      'componentFilters'
-    ],
-    data() {
-      return {
-        showFilter: false
-      }
-    }
+export default {
+  name: "table-filter",
+  props: ["componentFilters"],
+  data() {
+    return {
+      showFilter: false
+    };
   }
+};
 </script>
